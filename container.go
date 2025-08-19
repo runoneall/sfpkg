d570714652bd91b1ef8ae92a994300fb2802e2b0
runoneall <runoobsteve@hotmail.com>
2025-08-19 23:29:39 +0800

    remove debug opttion

--- container.go
+++ container.go
@@ -4,15 +4,10 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 func c_run(args []string) error {
-	real_args := append([]string{"UDOCKER_LOGLEVEL=0", "/usr/bin/udocker", "--allow-root"}, args...)
-
-	if os.Getenv("SFPKG_DEBUG") == "on" {
-		fmt.Println(">>>", strings.Join(real_args, " "))
-	}
+	real_args := append([]string{"UDOCKER_LOGLEVEL=2", "/usr/bin/udocker", "--allow-root"}, args...)
 	cmd := exec.Command(real_args[0], real_args[1:]...)
 
 	cmd.Stdin = os.Stdin
